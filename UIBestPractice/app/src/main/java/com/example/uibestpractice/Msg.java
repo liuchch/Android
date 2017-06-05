d3a680d0e39353453492197390192a22498905c1
Andy <842130398@qq.com>
2017-06-05 20:21:27 +0800

    基本完成UIBestPractice的代码，还有一个bug尚未修复

---
+++ UIBestPractice/app/src/main/java/com/example/uibestpractice/Msg.java
@@ -0,0 +1,27 @@
+package com.example.uibestpractice;
+
+/**
+ * Created by Andy on 2017/6/5.
+ */
+
+public class Msg {
+
+    public static final int TYPE_RECEIVED = 0;
+    public static final int TYPE_SENT = 1;
+
+    private String content;
+    private int type;
+
+    private Msg(String content, int type) {
+        this.content = content;
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getType() {
+        return type;
+    }
+}
