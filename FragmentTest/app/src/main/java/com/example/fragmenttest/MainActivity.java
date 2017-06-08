55e86ea119e0c928573dd3b18437f8af1d2a2d03
Andy <842130398@qq.com>
2017-06-08 18:27:16 +0800

    动态加载布局以及large限定符和最小宽度限定符

--- FragmentTest/app/src/main/java/com/example/fragmenttest/MainActivity.java
+++ FragmentTest/app/src/main/java/com/example/fragmenttest/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.fragmenttest;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -31,11 +29,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void replaceFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.right_layout, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction transaction = fragmentManager.beginTransaction();
+//        transaction.replace(R.id.right_layout, fragment);
+//        transaction.addToBackStack(null);
+//        transaction.commit();
     }
 
 
